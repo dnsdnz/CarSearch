40ff35453ea18e1086291f83a6436de22e759a8c
dnsdnz <berfz23@gmail.com>
2020-06-11 17:31:52 +0300

    Read file and main controller updated

--- src/main/java/com/denizcakmak/carsearch/controller/MainController.java
+++ src/main/java/com/denizcakmak/carsearch/controller/MainController.java
@@ -1,4 +1,22 @@
 package com.denizcakmak.carsearch.controller;
 
+import com.denizcakmak.carsearch.model.Car;
+import com.denizcakmak.carsearch.service.ReadFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController  //Creating controller class
+@RequestMapping("/cars") //Mapping the bean's method, work for coming request
 public class MainController {
+
+    @Autowired  //Inject bean to another bean
+    ReadFile readFile;
+
+
+
 }
--- src/main/java/com/denizcakmak/carsearch/service/ReadFile.java
+++ src/main/java/com/denizcakmak/carsearch/service/ReadFile.java
@@ -1,31 +1,33 @@
 package com.denizcakmak.carsearch.service;
 
-
 import com.denizcakmak.carsearch.model.Car;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component  //Car class is a spring Bean, bean: Spring container,manager
 public class ReadFile {
-    List<Car> carInfos ;
+    List<Car> carInfo;
 
     public void readFile(String path){
 
-        carInfos = new ArrayList<>();
+        carInfo = new ArrayList<>();
 
         File file = new File(path);
         String str;
-        BufferedReader bufferedReader;
+        BufferedReader bufferedReader;  //buffered reader for reading file
 
         {
             try {
                 bufferedReader = new BufferedReader(new FileReader(file));
 
-                while((str = bufferedReader.readLine())!=null){
+                while((str = bufferedReader.readLine())!=null){  //check the file is empty or not
 
+                    String str2 [] = str.split(";");
+                    carInfo.add(new Car(str2[0],str2[1],str2[2]));
                 }
 
             } catch (FileNotFoundException e) {
@@ -36,4 +38,34 @@ public class ReadFile {
         }
     }
 
+    public List<Car> search(String criteria, String key){  //search with criteria and key
+        List<Car> cars = new ArrayList<>();
+
+        readFile("/Users/macbookair/Downloads/carInfo.txt"); //txt file path
+
+        switch (criteria){
+            case "BRAND":
+                cars = carInfo.stream().filter(car -> car.getBrand().contains(key)).collect(Collectors.toList());
+                break;
+            case "MODEL":
+                cars = carInfo.stream().filter(car -> car.getModel().contains(key)).collect(Collectors.toList());
+                break;
+            case "CLASS":
+                cars = carInfo.stream().filter(car -> car.getCarClass().contains(key)).collect(Collectors.toList());
+                break;
+            case "ALL":
+                cars = carInfo.stream().filter(car -> car.getBrand().contains(key)
+                        || car.getCarClass().contains(key)
+                        || car.getModel().contains(key)).collect(Collectors.toList());
+                break;
+            default:
+                System.out.println("Not found");
+        }
+
+        cars.forEach(car -> {
+          System.out.println(car);
+        });
+
+        return cars;
+    }
 }
