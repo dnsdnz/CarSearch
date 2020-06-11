da404a3f02f6ed3f4a5677b42f2339a49a90fa3f
dnsdnz <berfz23@gmail.com>
2020-06-11 17:16:54 +0300

    Read file class created, buffered reader added

--- src/main/java/com/denizcakmak/carsearch/service/ReadFile.java
+++ src/main/java/com/denizcakmak/carsearch/service/ReadFile.java
@@ -1,4 +1,39 @@
 package com.denizcakmak.carsearch.service;
 
+
+import com.denizcakmak.carsearch.model.Car;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component  //Car class is a spring Bean, bean: Spring container,manager
 public class ReadFile {
+    List<Car> carInfos ;
+
+    public void readFile(String path){
+
+        carInfos = new ArrayList<>();
+
+        File file = new File(path);
+        String str;
+        BufferedReader bufferedReader;
+
+        {
+            try {
+                bufferedReader = new BufferedReader(new FileReader(file));
+
+                while((str = bufferedReader.readLine())!=null){
+
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
