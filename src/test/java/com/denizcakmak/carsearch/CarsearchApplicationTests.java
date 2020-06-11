3e9b332b21c3a3424a864b53e4a871c848991231
dnsdnz <berfz23@gmail.com>
2020-06-11 16:25:34 +0300

    First commit

---
+++ src/test/java/com/denizcakmak/carsearch/CarsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denizcakmak.carsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
